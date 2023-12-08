dfeba3d24deb1ced91cff1c86b2038bf0088eb24
Victor Vlădulescu <anthoillogical@gmail.com>
2023-12-08 23:05:01 +0200

    Remove newline from dbstring

--- backend/singletons.py
+++ backend/singletons.py
@@ -2,7 +2,7 @@ from sqlalchemy import *
 
 try:
     f = open("credentials.txt", "r")
-    dbstring = f.readline()
+    dbstring = f.read().replace("\n", "")
     engine = create_engine(dbstring, echo=False)
 except Exception as e:
     print(e)
