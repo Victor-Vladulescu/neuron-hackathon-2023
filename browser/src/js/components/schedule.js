928cbf93020dbb24ecd11bd7a50694856ea78f0b
zShax <71908965+zShax@users.noreply.github.com>
2023-12-08 22:44:20 +0200

    polished the app a little

--- browser/src/js/components/schedule.js
+++ browser/src/js/components/schedule.js
@@ -1,11 +1,10 @@
 function scheduleDropdown() {
   const dropdownButton = document.getElementById('dropdownButton');
-  const dropdownSchedule = document.getElementById(
-    'menu__navbar__dropdown-list'
-  );
+  const dropdownSchedule = document.getElementById('dropdownList');
 
   dropdownButton.classList.toggle('dropdownAnimation');
-  dropdownSchedule.classList.toggle('dropdownAnimation');
+  dropdownSchedule.classList.toggle('dropdownVisibility');
+  dropdownSchedule.classList.toggle('menu__navbar__dropdown-list');
 }
 
 document
