7ec4a9fff0850c6ad37b625f6246fb239373a0d4
Victor Vlădulescu <anthoillogical@gmail.com>
2023-12-09 17:42:31 +0200

    Made troubled transition

---
+++ mobile/www/js/components/status-change.js
@@ -0,0 +1,23 @@
+// Find the checkbox input
+const locationCheckbox = document.getElementById('locationCheckbox');
+
+function openChange() {
+  // Find the label element that contains the pseudo-element after
+  const button = document.getElementById('locationStatus');
+
+  // If input is checked
+  if (locationCheckbox.checked) {
+    console.log('Hello from Florin1');
+    //   Set after to "Activated"
+    button.setAttribute('data-value', 'ACTIVAT');
+  } else {
+    console.log('Hello from Florin2');
+    //   Otherwise set after to "Dectivated"
+    button.setAttribute('data-value', 'DEZACTIVAT');
+  }
+}
+
+// Only run the function on change of checkbox input
+locationCheckbox.addEventListener('change', () => {
+  openChange();
+});
--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -1,66 +1,7 @@
-let btnState = false
-let longHTML
-let latHTML
-let btnHTML
-let tramInput
-let sendLocationInterval
-let currentLocation
+console.log('Hello World!');
 
-document.addEventListener('deviceready', onDeviceReady, false)
-
-function onDeviceReady() {    
-    longHTML = document.getElementById('long')
-    latHTML = document.getElementById('lat')
-    btnHTML = document.getElementById('state-btn')
-    tramInput = document.getElementById('tram-select')
-
-    navigator.geolocation.watchPosition(
-        (position) => {
-            currentLocation = position
-            let lat = position.coords.latitude
-            let long = position.coords.longitude
-
-            latHTML.innerText = 'Latitude: ' + lat
-            longHTML.innerText = 'Longitude: ' + long
-        }, 
-        (error) => {
-            console.log(error)
-            longHTML.innerText = error.message
-            latHTML.innerText = error.message
-        },
-        {timeout: 30000, enableHighAccuracy: true, maximumAge: 10000}
-    );
-}
-
-function toggleSendLocation() {
-
-    if (btnState) {
-        btnState = false
-        btnHTML.innerText = "Send location"
-        clearInterval(sendLocationInterval)
-    }
-    else {
-
-        // must have selected tram
-        if (tramInput.value && currentLocation) {
-            btnState = true
-            btnHTML.innerText = "Stop sending location"
-            sendLocationInterval = setInterval(() => sendLocation(currentLocation), 1000)
-        }
-        else {
-            alert("Either you haven't selected a tram, or we can't get a fix on your location.")
-        }
-    } 
-}
-
-function sendLocation(position) {
-    fetch("http://uav-easy-exams.xyz:5000/api/vehicle/" + tramInput.value + "/update", {
-        method: "PUT",
-        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-        body: JSON.stringify({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            timestamp: new Date().toISOString()
-        })
-    })
-}
+export let baseURL = 'http://uav-easy-exams.xyz:5000/api';
+export var map = L.map('map', { zoomControl: false }).setView(
+  [46.186504598160354, 21.31672212393795],
+  11
+);
