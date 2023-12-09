cda6aee4d4aea6d4226b221fe290578f8cb07d27
zShax <71908965+zShax@users.noreply.github.com>
2023-12-09 15:53:01 +0200

    finished vehicle put fetch request

---
+++ browser/vatman-app/src/js/components/vehicle-selection.js
@@ -0,0 +1,53 @@
+// FIXME Fake Position Template FIXME
+const currentPosition = {
+  id: 0,
+  coords: {
+    latitude: 123,
+    longitude: 321,
+  },
+  timestamp: '2023-12-01T11:01:00.123Z',
+};
+
+// Vehicle ID Select Input
+const selectElement = document.getElementById('vehicleId');
+
+var newValueMine;
+
+// Send the selected input function
+function sendOption() {
+  const selectedValue = selectElement.value;
+  newValueMine = selectedValue;
+
+  if (selectedValue === '') {
+    console.log('No vehicle was selected');
+    localStorage.removeItem('tramInput');
+  } else {
+    console.log(`No° ${selectedValue} was selected`);
+    localStorage.setItem('tramInput', selectedValue);
+  }
+}
+
+// Run the function on each change of the input
+selectElement.addEventListener('change', () => {
+  sendOption();
+
+  setInterval(sendLocation(), 1000);
+});
+
+function sendLocation() {
+  fetch(
+    'http://uav-easy-exams.xyz:5000/api/vehicle/' + newValueMine + '/update',
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        latitude: currentPosition.coords.latitude,
+        longitude: currentPosition.coords.longitude,
+        timestamp: new Date().toISOString(),
+      }),
+    }
+  );
+}
